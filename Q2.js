78d8912bc7a98f46507926c2767bb25ad257b45b
MonjedNidal <monjed1010@gmail.com>
2023-09-13 19:25:53 +0300

    commit

---
+++ Q2.js
@@ -0,0 +1,10 @@
+const findMissingNumber = function (arr, n) {
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr.includes(i)) {
+      return i;
+    }
+  }
+  return n;
+};
+
+console.log(findMissingNumber([3, 0, 1], 3));
